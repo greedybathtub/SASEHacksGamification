e674251c93b24198c8b1a57fb8f9d5d7ee83e64b
Giavanna  Flaherty <graeflaherty@gmail.com>
2026-03-08 03:20:09 -0400

    protect my key

--- addMongo.py
+++ addMongo.py
@@ -1,7 +1,7 @@
 #Database connection and collection setup for Paws and Pages application
 from pymongo import MongoClient
 
-uri = "mongodb+srv://graeflaherty_db_user:oSnhJJzDbr6P6mDh@pawsandpages.1vd632v.mongodb.net/?appName=PawsandPages"
+uri = MONGO_URI
 
 client = MongoClient(uri)
 db = client["PawsandPages"]
